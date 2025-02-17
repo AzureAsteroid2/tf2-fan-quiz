70150b6f4edc80f19aac637ad13062f579588cc1
AzureAsteroid2 <77765010+AzureAsteroid2@users.noreply.github.com>
2025-02-16 22:39:22 -0700

    mmmmm heavy tf2. Form is being fleshed out

---
+++ src/components/Question.jsx
@@ -0,0 +1,55 @@
+import defaultImage from "../assets/heavy_stare.png";
+import { useState } from 'react';
+
+function Question({ question, answers, picture = defaultImage, onNextQuestion }) {
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+    const handleAnswerSelect = (answer) => {
+        setSelectedAnswer(answer);
+    };
+
+    const handleNext = () => {
+        if (selectedAnswer) {
+            onNextQuestion(selectedAnswer.value);
+            setSelectedAnswer(null);
+        }
+    };
+
+    return (
+        <>
+            <div className="question-container">
+                <h2>{question}</h2>
+                <img
+                    src={picture}
+                    alt="Question"
+                    className="question-image"
+                />
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <div className="answers-container">
+                        {answers.map((answer, index) => (
+                            <button
+                                key={index}
+                                type="button"
+                                className={`answer-button ${selectedAnswer === answer ? 'selected' : ''}`}
+                                onClick={() => handleAnswerSelect(answer)}
+                            >
+                                {answer.text}
+                            </button>
+                        ))}
+                    </div>
+                    <div>
+                        <button
+                            className="next-button"
+                            onClick={handleNext}
+                            disabled={!selectedAnswer}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default Question;
